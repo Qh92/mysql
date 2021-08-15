1de3ada99b5e4b2888e74a4146af5ef4ac9decde
Qh92 <771600967@qq.com>
2021-08-15 22:36:03 +0800

    feat:添加思维导图和初始化sql

--- mysql/doc/init_data.sql
+++ mysql/doc/init_data.sql
@@ -28,3 +28,175 @@ insert into tbl_emp(name,deptId) values('w6',2);
 insert into tbl_emp(name,deptId) values('s7',3);
 insert into tbl_emp(name,deptId) values('s8',4);
 insert into tbl_emp(name,deptId) values('s9',51);
+
+#一.单表索引优化案例
+CREATE TABLE IF NOT EXISTS `article`(
+`id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`author_id` INT (10) UNSIGNED NOT NULL,
+`category_id` INT(10) UNSIGNED NOT NULL ,
+`views` INT(10) UNSIGNED NOT NULL ,
+`comments` INT(10) UNSIGNED NOT NULL,
+`title` VARBINARY(255) NOT NULL,
+`content` TEXT NOT NULL
+);
+INSERT INTO `article`(`author_id`,`category_id` ,`views` ,`comments` ,`title` ,`content` )VALUES
+(1,1,1,1,'1','1'),
+(2,2,2,2,'2','2'),
+(3,3,3,3,'3','3');
+
+explain select id , author_id from article where category_id = 1 and comments > 1 order by views limit 1;
+#创建索引
+create index idx_article_ccv on article(category_id,comments,views);
+#索引创建并未解决问题
+drop index idx_article_ccv on article;
+#再次创建索引优化
+create index idx_article_cv on article(category_id,views);
+
+#二.两张表
+CREATE TABLE IF NOT EXISTS `class`(
+`id` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`card` INT (10) UNSIGNED NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `book`(
+`bookid` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`card` INT (10) UNSIGNED NOT NULL
+);
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO class(card)VALUES(FLOOR(1+(RAND()*20)));
+
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO book(card)VALUES(FLOOR(1+(RAND()*20)));
+
+explain select * from class left join book on class.card = book.card;
+
+#现在是不确定跟哪张表创建索引,两张表都分别 创建索引进行测试
+#给book表添加索引
+alter table `book` add index y (`card`);
+drop index y on book;
+#给class表添加索引
+alter table `class` add index y (`card`);
+drop index y on class;
+#结论:左连接在右表建索引,右连接在左表减索引
+
+#三.三张表关联
+CREATE TABLE IF NOT EXISTS `phone`(
+`phoneid` INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`card` INT (10) UNSIGNED NOT NULL
+)ENGINE = INNODB;
+
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+INSERT INTO phone(card)VALUES(FLOOR(1+(RAND()*20)));
+
+explain select * from class left join book on class.card = book.card left join phone on book.card = phone.card;
+
+#按两表左右关联查询的分析,先建如下的索引
+alter table `phone` add index z(`card`);
+alter table `book` add index y(`card`);
+
+
+#索引失效
+CREATE TABLE staffs(
+id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(24)NOT NULL DEFAULT'' COMMENT'姓名',
+`age` INT NOT NULL DEFAULT 0 COMMENT'年龄',
+`pos` VARCHAR(20) NOT NULL DEFAULT'' COMMENT'职位',
+`add_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT'入职时间'
+)CHARSET utf8 COMMENT'员工记录表';
+INSERT INTO staffs(`name`,`age`,`pos`,`add_time`) VALUES('z3',22,'manager',NOW());
+INSERT INTO staffs(`name`,`age`,`pos`,`add_time`) VALUES('July',23,'dev',NOW());
+INSERT INTO staffs(`name`,`age`,`pos`,`add_time`) VALUES('2000',23,'dev',NOW());
+
+ALTER TABLE staffs ADD INDEX index_staffs_nameAgePos(`name`,`age`,`pos`);
+
+
+#like索引失效问题
+CREATE TABLE `tbl_user`(
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(20) DEFAULT NULL,
+`age`INT(11) DEFAULT NULL,
+`email` VARCHAR(20) DEFAULT NULL,
+PRIMARY KEY(`id`)
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO tbl_user(`name`,`age`,`email`)VALUES('1aa1',21,'a@163.com');
+INSERT INTO tbl_user(`name`,`age`,`email`)VALUES('2bb2',23,'b@163.com');
+INSERT INTO tbl_user(`name`,`age`,`email`)VALUES('3cc3',24,'c@163.com');
+INSERT INTO tbl_user(`name`,`age`,`email`)VALUES('4dd4',26,'d@163.com');
+
+
+#面试题讲解
+create table test03(
+id int primary key not null auto_increment,
+c1 char(10),
+c2 char(10),
+c3 char(10),
+c4 char(10),
+c5 char(10));
+
+insert into test03(c1,c2,c3,c4,c5) values ('a1','a2','a3','a4','a5');
+insert into test03(c1,c2,c3,c4,c5) values ('b1','b2','b3','b4','b5');
+insert into test03(c1,c2,c3,c4,c5) values ('c1','c2','c3','c4','c5');
+insert into test03(c1,c2,c3,c4,c5) values ('d1','d2','d3','d4','d5');
+insert into test03(c1,c2,c3,c4,c5) values ('e1','e2','e3','e4','e5');
+
+create index idx_test03_c1234 on test03(c1,c2,c3,c4);
+
+
+
+
+
+
